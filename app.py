1e592038e234a985da0ff02f556e9010c6c8e7d4
Emma C <c.emma2112@gmail.com>
2026-05-08 20:45:21 +0100

    testing several routes & adding comments of findings

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def test():
     return 'hello world'
 
+@app.route('/about') # decorator - must be directly above the function I want to call
+def aboutTest(): # connected to function
+    return 'this is a flask app'
+
 # run
 if __name__ == '__main__':
     app.run(debug=True)
