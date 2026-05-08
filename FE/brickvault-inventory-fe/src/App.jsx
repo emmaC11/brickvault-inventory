3c9f6cfd026dcc37f126fe7bd4aa4e0196344ee9
Emma C <c.emma2112@gmail.com>
2026-05-08 21:12:52 +0100

    update state variables & add useEffect to fetch sample flask API

--- FE/brickvault-inventory-fe/src/App.jsx
+++ FE/brickvault-inventory-fe/src/App.jsx
@@ -2,7 +2,22 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('')
+  const [error, setError] = useState('')
+
+  // fetch data from flask BE 
+  useEffect(() =>
+  {
+    fetch('http://localhost:5000/api/about')
+    .then(response => response.json())
+    .then(data => {
+      setMessage(data.message)
+    })
+    .catch(err => {
+      setError('cannot connect to flask BE')
+      console.error(err)
+    })
+  }, [])
 
   return (
     <>
