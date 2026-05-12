1a72a3ec5e0b3d563d4a5be09b08f553e4c86d09
Emma C <c.emma2112@gmail.com>
2026-05-12 21:01:23 +0100

    add post route to add data to sets

--- BE/routes/sets.py
+++ BE/routes/sets.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from database import db
 from models import LegoSet
 
 # splits logic into smaller pieces - can have multiple blueprints for different resources, prevents all logic from being in app.py
@@ -9,3 +10,11 @@ def get_sets():
     sets = LegoSet.query.all()
     sets_list = [set.to_dict() for set in sets]
     return jsonify(sets_list)
+
+@sets_bp.route('/sets', methods=['POST'])
+def create_set():
+    data = request.get_json()
+    new_set = LegoSet(name=data['name'], price=data['price'], set_number=data['set_number'])
+    db.session.add(new_set)
+    db.session.commit()
+    return jsonify(new_set.to_dict()), 201
