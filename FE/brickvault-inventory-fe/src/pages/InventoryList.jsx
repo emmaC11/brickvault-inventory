d90399e93402da73feaf4eeaab41f45513168449
Emma C <c.emma2112@gmail.com>
2026-05-16 16:17:03 +0100

    add logic to fetch sets & assign to state variable

--- FE/brickvault-inventory-fe/src/pages/InventoryList.jsx
+++ FE/brickvault-inventory-fe/src/pages/InventoryList.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
 
 export default function InventoryList() {
+
+  const [sets, setSets] = React.useState([])
+  const [error, setError] = React.useState('')
+
+  useEffect(() => {
+    fetchSets()
+  }, [])
+
+  const fetchSets = async () => {
+    try {
+      const res = await fetch('http://localhost:5000/sets')
+      const data = await res.json()
+      setSets(data)
+    } catch (err) {
+      setError(err.message)
+      console.error('error fetching sets:', err)
+    }
+  }
   return (
     <div>
       
