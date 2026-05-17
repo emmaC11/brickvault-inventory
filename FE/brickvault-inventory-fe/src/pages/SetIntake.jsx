94e5a1d40c9dd1d0292a882c1e7a19b18622b57d
Emma C <c.emma2112@gmail.com>
2026-05-17 18:14:21 +0100

    add state variables to SetIntake.jsx

--- FE/brickvault-inventory-fe/src/pages/SetIntake.jsx
+++ FE/brickvault-inventory-fe/src/pages/SetIntake.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
+const [setNumber, setSetNumber] = useState('');
+const [formData, setFormData] = useState({
+  setNumber: '',
+  name: '',
+  year: '',
+  numParts: '',
+  price: '',
+  notes: '',
+  stock:''
+});
+
 export default function SetIntake() {
   return (
     <div>
