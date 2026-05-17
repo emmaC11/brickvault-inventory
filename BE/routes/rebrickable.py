e1d128ea24e12c6cecb2cfbfcf4f70ec7fecc29e
Emma C <c.emma2112@gmail.com>
2026-05-17 17:13:50 +0100

    add logic to fetch from rebrickable API

---
+++ BE/routes/rebrickable.py
@@ -0,0 +1,18 @@
+from env import REBRICKABLE_API_KEY
+from flask import Blueprint, jsonify, request
+import requests
+
+rebrickable_bp = Blueprint('rebrickable', __name__)
+
+@rebrickable_bp.route('api/rebrickable/sets/<set_number>', methods=['GET'])
+def get_set_details_from_rebrickable(set_number):
+        url = 'https://rebrickable.com/api/v3/lego/sets/{set_number}'
+        headers = {'Authorization': {REBRICKABLE_API_KEY}}
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code !=200:
+            return jsonify({'failed to fetch data from rebrickable API'}), response.status_code
+
+        if response.status_code == 404:
+            return jsonify({'set not found in rebrickable API'}), 404
