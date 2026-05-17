bb52ecdb41e16ad38c796f55ec60c46ec4bbb8b4
Emma C <c.emma2112@gmail.com>
2026-05-17 17:24:39 +0100

    move logic into try catch

--- BE/routes/rebrickable.py
+++ BE/routes/rebrickable.py
@@ -6,25 +6,29 @@ rebrickable_bp = Blueprint('rebrickable', __name__)
 
 @rebrickable_bp.route('api/rebrickable/sets/<set_number>', methods=['GET'])
 def get_set_details_from_rebrickable(set_number):
-        url = 'https://rebrickable.com/api/v3/lego/sets/{set_number}'
-        headers = {'Authorization': {REBRICKABLE_API_KEY}}
+        try: 
+            url = 'https://rebrickable.com/api/v3/lego/sets/{set_number}'
+            headers = {'Authorization': {REBRICKABLE_API_KEY}}
 
-        response = requests.get(url, headers=headers)
+            response = requests.get(url, headers=headers)
 
-        if response.status_code !=200:
-            return jsonify({'failed to fetch data from rebrickable API'}), response.status_code
+            if response.status_code !=200:
+                return jsonify({'failed to fetch data from rebrickable API'}), response.status_code
 
-        if response.status_code == 404:
-            return jsonify({'set not found in rebrickable API'}), 404
-        
-        # parse response to dict & map to LegoSet fields
-        data = response.json()
+            if response.status_code == 404:
+                return jsonify({'set not found in rebrickable API'}), 404
+            
+            # parse response to dict & map to LegoSet fields
+            data = response.json()
 
-        set_data = {
-            'name': data.get('name'),
-            'set_number': data.get('set_num'),
-            'year': data.get('year'),
-            'num_parts': data.get('num_parts'),
-        }
+            set_data = {
+                'name': data.get('name'),
+                'set_number': data.get('set_num'),
+                'year': data.get('year'),
+                'num_parts': data.get('num_parts'),
+            }
 
-        return jsonify(set_data), 200   
+            return jsonify(set_data), 200
+
+        except Exception as error:
+            return jsonify({'error in rebrickable.py': str(error)}), 500   
