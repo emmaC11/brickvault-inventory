20271d81e5370baf473f22f5e8a8f758bdb93cec
Emma C <c.emma2112@gmail.com>
2026-05-18 20:15:21 +0100

    add function to handle form submission - send to our post endpoint

--- FE/brickvault-inventory-fe/src/pages/SetIntake.jsx
+++ FE/brickvault-inventory-fe/src/pages/SetIntake.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const [setNumber, setSetNumber] = useState('');
+const [success, setSuccess] = useState('');
 const [error, setError] = useState('');
 const [formData, setFormData] = useState({
   setNumber: '',
@@ -33,6 +34,40 @@ const fetchSetData = async () =>
     catch (err) {
       setError(err.message);
       console.error('error fetching from rebrickable api from SetIntake.jsx', err);
+    }
+}
+
+// handle form submissions - send form data to our post endpoint
+const onSubmit = async (e) => {
+  e.preventDefault(); // prevent default reload
+
+  try
+  {
+    const res = await fetch('http://localhost:5000/api/sets', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+  },
+    body: JSON.stringify({
+      name: formData.name,
+      set_number: formData.setNumber,
+      year: formData.year,
+      num_parts: formData.numParts,
+      price: formData.price,
+      notes: formData.notes,
+      stock: formData.stock
+    })
+});
+
+    if (!res.ok) {
+      throw new Error('Failed to add set to inventory');
+    }
+
+    setSuccess('set added successfully');
+  }
+    catch (err) {
+      setError(err.message);
+      console.error('error submitting form data from SetIntake.jsx', err);
     }
 }
 
