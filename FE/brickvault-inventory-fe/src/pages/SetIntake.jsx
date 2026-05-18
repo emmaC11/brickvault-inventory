8a5f283b8137900af3956c9bd7bf1b5784450a90
Emma C <c.emma2112@gmail.com>
2026-05-18 20:04:35 +0100

    add function to fetch data from our rebrickable API

--- FE/brickvault-inventory-fe/src/pages/SetIntake.jsx
+++ FE/brickvault-inventory-fe/src/pages/SetIntake.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const [setNumber, setSetNumber] = useState('');
+const [error, setError] = useState('');
 const [formData, setFormData] = useState({
   setNumber: '',
   name: '',
@@ -10,6 +11,30 @@ const [formData, setFormData] = useState({
   notes: '',
   stock:''
 });
+
+// fetch data from rebrickabke api
+const fetchSetData = async () =>
+{
+  if (!setNumber) {
+    setError('Please enter a set number');
+    return;
+  }
+
+  try {
+    const res = await fetch(`http://localhost:5000/api/rebrickable/sets/${setNumber}`);
+
+    if (!res.ok) {
+      throw new Error('Set not found');
+    }
+
+    const data = await res.json(); 
+  }
+
+    catch (err) {
+      setError(err.message);
+      console.error('error fetching from rebrickable api from SetIntake.jsx', err);
+    }
+}
 
 export default function SetIntake() {
   return (
