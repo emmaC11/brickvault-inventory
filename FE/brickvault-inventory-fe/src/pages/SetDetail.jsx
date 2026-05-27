f0a8641edeae59059cf9f82cbe284662a7717b8b
Emma C <c.emma2112@gmail.com>
2026-05-27 21:56:45 +0100

    set state variables

--- FE/brickvault-inventory-fe/src/pages/SetDetail.jsx
+++ FE/brickvault-inventory-fe/src/pages/SetDetail.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useState, useEffect, useParams } from 'react'
 
 export default function SetDetail() {
+  // setId passed from InventoryList.jsx (view button)
+  const { setId } = useParams()
+  const [setInfo, setSetInfo] = useState('')
+  const [error, setError] = useState('')
   return (
     <div>
       
