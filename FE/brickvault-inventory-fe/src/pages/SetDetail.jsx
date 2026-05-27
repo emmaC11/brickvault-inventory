eac74020ab91490ee30269e54681ef726792ffe8
Emma C <c.emma2112@gmail.com>
2026-05-27 22:01:02 +0100

    add logic to fetch set details from route & assign to state variable

--- FE/brickvault-inventory-fe/src/pages/SetDetail.jsx
+++ FE/brickvault-inventory-fe/src/pages/SetDetail.jsx
@@ -6,6 +6,24 @@ export default function SetDetail() {
   const { setId } = useParams()
   const [setInfo, setSetInfo] = useState('')
   const [error, setError] = useState('')
+
+  useEffect(() => {
+    fetchSetDetails()
+  }, [setId])
+
+  const fetchSetDetails = async () => {
+    try {
+      const res = await fetch(`http://localhost:5000/sets/${setId}`)
+      if(!res.ok) {
+        throw new Error('Error fetching set details')
+      }
+      const data = await res.json()
+      setSetInfo(data)
+    } catch (err) {
+      setError(err.message)
+      console.error('error fetching set details:', err)
+    }
+  }
   return (
     <div>
       
