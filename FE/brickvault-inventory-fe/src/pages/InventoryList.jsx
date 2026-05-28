6a432a2a2b68ecff02dfcbd137d8a4caf5c26634
Emma C <c.emma2112@gmail.com>
2026-05-28 08:30:41 +0100

    add click event on delete button to call delete function

--- FE/brickvault-inventory-fe/src/pages/InventoryList.jsx
+++ FE/brickvault-inventory-fe/src/pages/InventoryList.jsx
@@ -102,7 +102,7 @@ export default function InventoryList() {
                   <Button size="small" variant="text" color="primary" sx={{ ml: 1 }}>
                     Edit
                   </Button>
-                  <Button size="small" variant="text" color="error" sx={{ ml: 1 }}>
+                  <Button size="small" variant="text" color="error" sx={{ ml: 1 }} onClick={() => deleteSet(set.id)}>
                     Delete
                   </Button>
                 </TableCell>
