f540ac53ab91c2c3f919f1afe874b656771db118
Emma C <c.emma2112@gmail.com>
2026-05-29 23:38:55 +0100

    add state variables for SetUpdate.jsx

--- FE/brickvault-inventory-fe/src/pages/SetUpdate.jsx
+++ FE/brickvault-inventory-fe/src/pages/SetUpdate.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function SetUpdate() {
+    const { setId } = useParams()
+    const [error, setError] = useState('')
+    // initiaise form data with empty values, will update with set details on load
+    const [formData, setFormData] = useState({
+        name: '',
+        set_number: '',
+        year: '',
+        num_parts: '',
+        price: '',
+        stock: '',
+        notes: ''
+    })
+    
   return (
     <div>
       
