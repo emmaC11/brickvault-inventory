a7ac8c8e7172f34aa731e1f00a6ae61e77c1c353
Emma C <c.emma2112@gmail.com>
2026-06-02 00:36:35 +0100

    add test data to test_customers.py

---
+++ BE/tests/test_customers.py
@@ -0,0 +1,15 @@
+SAMPLE_CUSTOMER = {
+    'f_name': 'John',
+    'l_name': 'Doe',
+    'email': 'john.doe@myemail.com'
+}
+
+SAMPLE_SET = {
+    'name': 'Millennium Falcon',
+    'price': 99.99,
+    'set_number': '75192',
+    'year': 2017,
+    'num_parts': 7541,
+    'stock': 2,
+    'condition': 'New'
+}
